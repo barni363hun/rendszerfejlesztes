07ff2cf1a2fb77886e28c356df132d85afc93c82
AbelNemeth <abelnemeth@protonmail.com>
2024-04-03 17:48:58 +0200

    created classes for tables

---
+++ ClassLibrary/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    internal class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeId { get; set; }
+        [ForeignKey("TypeId")]
+        public Project_Type Type { get; set; }
+        public string Description { get; set; }
+        public List<Developer> Developers { get; set; }
+    }
+}
---
+++ ClassLibrary/Models/Project_Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    internal class Project_Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
